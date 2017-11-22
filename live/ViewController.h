a9e4043faa5f46a5ae88ea1cccc65fe0f83c1866
huyong668 <229376483@qq.com>
2017-11-22 16:19:47 +0800

    Initial Commit

---
+++ live/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  live
+//
+//  Created by Jack on 2017/11/22.
+//  Copyright © 2017年 胡勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ live/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  live
+//
+//  Created by Jack on 2017/11/22.
+//  Copyright © 2017年 胡勇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
