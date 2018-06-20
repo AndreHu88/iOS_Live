b42507ff055d5d6b41c9235a205d9e28885cdf10
Jack <229376483@qq.com>
2018-06-20 11:26:41 +0800

    alter videoEncode

--- live/Module/LaunchLive/Controller/HYVideoHardEncodeVC.m
+++ live/Module/LaunchLive/Controller/HYVideoHardEncodeVC.m
@@ -22,18 +22,6 @@
  CMClock
  
  CMTimebase: 关于CMClock的一个控制视图,包含CMClock、时间映射(Time mapping)、速率控制(Rate control)
- 
- H264中定义三种帧:
- I帧：完整编码的帧叫I帧
- P帧：参考之前的I帧生成的只包含差异部分编码的帧叫P帧
- B帧：参考前后的帧编码的帧叫B帧
- 
- H264采用的核心算法是帧内压缩和帧间压缩
- 帧内压缩是生成I帧的算法
- 帧间压缩是生成B帧和P帧的算法
- 
- H.264详细讲解blog
- https://www.jianshu.com/p/9edd9619e233?utm_campaign=maleskine&utm_content=note&utm_medium=seo_notes&utm_source=recommendation
  */
 
 #import "HYVideoHardEncodeVC.h"
@@ -97,6 +85,10 @@
 
 - (void)setupSession{
     
+    _session = [AVCaptureSession new];
+    //设置采集分辨率
+    _session.sessionPreset = AVCaptureSessionPreset1920x1080;
+    
     AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
     NSError *inputError;
     AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&inputError];
@@ -104,18 +96,19 @@
         [JRToast showWithText:inputError.description duration:2];
         return;
     }
+    //设置session的input output
+    if ([_session canAddInput:input])           [_session addInput:input];
     
     AVCaptureVideoDataOutput *videoOutput = [AVCaptureVideoDataOutput new];
-    [videoOutput setAlwaysDiscardsLateVideoFrames:YES];
+    [videoOutput setAlwaysDiscardsLateVideoFrames:NO];
     [videoOutput setSampleBufferDelegate:self queue:_captureQueue];
     [videoOutput setVideoSettings:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:kCVPixelFormatType_420YpCbCr8BiPlanarFullRange] forKey:(id)kCVPixelBufferPixelFormatTypeKey]];
+    if ([_session canAddOutput:videoOutput])    [_session addOutput:videoOutput];
+
     
-    _session = [AVCaptureSession new];
-    //设置采集分辨率
-    _session.sessionPreset = AVCaptureSessionPresetHigh;
-    //设置session的input output
-    [_session addInput:input];
-    [_session addOutput:videoOutput];
+    AVCaptureConnection *connection = [videoOutput connectionWithMediaType:AVMediaTypeVideo];
+    connection.videoOrientation = AVCaptureVideoOrientationPortrait;
+    
     
     //设置预览层
     _previewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:_session];
@@ -123,21 +116,23 @@
     _previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
     [self.view.layer addSublayer:_previewLayer];
     
+    [_session startRunning];
+}
+
+- (void)setupFileHandle{
+    
     //创建文件，初始化fileHandle;
     NSString *file = [[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject] stringByAppendingPathComponent:@"test.h264"];
     [[NSFileManager defaultManager] removeItemAtPath:file error:nil];
     [[NSFileManager defaultManager] createFileAtPath:file contents:nil attributes:nil];
     _fileHandle = [NSFileHandle fileHandleForWritingAtPath:file];
-    
-    
-    [_session startRunning];
-    [self setupVideoToolbox];
 }
 
 - (void)stopCapture{
     
     [_session stopRunning];
     [_previewLayer removeFromSuperlayer];
+    [self endVideoToolbox];
     [_fileHandle closeFile];
     _fileHandle = nil;
 }
@@ -146,6 +141,8 @@
     
     dispatch_sync(_encodeQueue, ^{
         
+        [self setupFileHandle];
+
         int width = 720, height = 1280;
         OSStatus status = VTCompressionSessionCreate(NULL, width, height, kCMVideoCodecType_H264, NULL, NULL, NULL, encodeComplectionCallback, (__bridge void *)(self), &_encodeingSession);
         DLog(@"status code is %d",(int)status);
@@ -158,13 +155,13 @@
         VTSessionSetProperty(_encodeingSession, kVTCompressionPropertyKey_RealTime, kCFBooleanTrue);
         VTSessionSetProperty(_encodeingSession, kVTCompressionPropertyKey_ProfileLevel, kVTProfileLevel_H264_Baseline_AutoLevel);
         
-        //设置关键帧间隔（） 关键帧间隔越小越清晰
-        int frameInterval = 30;
+        //设置关键帧间隔（）关键字间隔越小越清晰
+        int frameInterval = 1;
         CFNumberRef frameIntervalRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &frameInterval);
         VTSessionSetProperty(_encodeingSession, kVTCompressionPropertyKey_MaxKeyFrameInterval, frameIntervalRef);
         
-        //设置期望帧率(每秒多少帧,如果帧率过低,会造成画面卡顿)
-        int fps = 30;
+        //设置期望帧率
+        int fps = 20;
         CFNumberRef fpsRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberIntType, &fps);
         VTSessionSetProperty(_encodeingSession, kVTCompressionPropertyKey_ExpectedFrameRate, fpsRef);
         
@@ -180,6 +177,7 @@
         
         //准备编码
         VTCompressionSessionPrepareToEncodeFrames(_encodeingSession);
+        
     });
 }
 
@@ -189,6 +187,9 @@
     VTCompressionSessionInvalidate(_encodeingSession);      //销毁session
     CFRelease(_encodeingSession);       //释放资源
     _encodeingSession = NULL;
+    
+    [_fileHandle closeFile];
+    _fileHandle = nil;
 }
 
 #pragma mark - encodeCallback
@@ -211,11 +212,10 @@ void encodeComplectionCallback(void * CM_NULLABLE outputCallbackRefCon,
     //将C语言指针转化为当前VC
     HYVideoHardEncodeVC *encoderVC = (__bridge HYVideoHardEncodeVC *)outputCallbackRefCon;
     CFDictionaryRef dictRef = CFArrayGetValueAtIndex(CMSampleBufferGetSampleAttachmentsArray(sampleBuffer, true), 0);
-    bool iskeyFrame = CFDictionaryContainsKey(dictRef, kCMSampleAttachmentKey_NotSync);
+    bool iskeyFrame = !CFDictionaryContainsKey(dictRef, kCMSampleAttachmentKey_NotSync);
     //判断是否为关键帧
     if (iskeyFrame) {
         
-        //获取编码后的信息
         CMFormatDescriptionRef formatRef = CMSampleBufferGetFormatDescription(sampleBuffer);
         size_t spsParameterSetSizeOut,spsParameterSetCountOut;
         const uint8_t *spsParameterSetPointerOut;
@@ -237,25 +237,18 @@ void encodeComplectionCallback(void * CM_NULLABLE outputCallbackRefCon,
         }
     }
     
-    //CMBlockBufferRef已经压缩的图像数据
     CMBlockBufferRef dataBuffer = CMSampleBufferGetDataBuffer(sampleBuffer);
     size_t bufferOffset = 0,lengthAtOffset,totalLength;
-    char *dataPointer;
+    char * dataPointer;
     OSStatus statusCode = CMBlockBufferGetDataPointer(dataBuffer, 0, &lengthAtOffset, &totalLength, &dataPointer);
     if (statusCode == noErr) {
         
-        /**
-         一个完整的H.264/ AVC bitstream是由多个NAL-units所组成的, NAL 网络提取层
-         H.264的功能分为两层，视频编码层（VCL）和网络提取层（NAL）VCL数据即被压缩编码后的视频数据序列。在VCL数据要封装到NAL单元中之后，才可以用来传输或存储。
-         */
-        
         static const int AVCCHeaderLength = 4; // 返回的NALU数据前四个字节不是0001的startcode，而是大端模式的帧长度length
-        //循环读取NALU数据
         while(totalLength - AVCCHeaderLength > bufferOffset) {
             
             uint32_t NALUUintLength = 0;
             memcpy(&NALUUintLength, dataPointer + bufferOffset, AVCCHeaderLength);
-            //从大端转系统端 以大端字节序表示。直接赋值给整型变量
+            //从大端转系统端
             NALUUintLength = CFSwapInt32BigToHost(NALUUintLength);
             NSData *data = [NSData dataWithBytes:(dataPointer + bufferOffset + AVCCHeaderLength) length:NALUUintLength];
             [encoderVC encodeData:data isKeyFrame:iskeyFrame];
@@ -273,6 +266,7 @@ void encodeComplectionCallback(void * CM_NULLABLE outputCallbackRefCon,
     const char bytes[] = "\x00\x00\x00\x01";
     size_t length = (sizeof bytes) - 1;
     NSData *ByteHeader = [NSData dataWithBytes:bytes length:length];
+    if(!_fileHandle) return;
     [_fileHandle writeData:ByteHeader];
     [_fileHandle writeData:spsData];
     [_fileHandle writeData:ByteHeader];
@@ -281,6 +275,7 @@ void encodeComplectionCallback(void * CM_NULLABLE outputCallbackRefCon,
 
 - (void)encodeData:(NSData *)data isKeyFrame:(BOOL)isKeyFrame{
     
+    DLog(@"getEncodedData:%d", (int)[data length]);
     if (_fileHandle) {
         
         const char bytes[] = "\x00\x00\x00\x01";
@@ -341,11 +336,15 @@ void encodeComplectionCallback(void * CM_NULLABLE outputCallbackRefCon,
 
 - (void)startRecordBtnAction{
     
+    if (!_isStartRecord) {
+        [self setupVideoToolbox];
+    }
+    else{
+        [self endVideoToolbox];
+    }
     _isStartRecord = !_isStartRecord;
     NSString *toastStr = _isStartRecord ? @"开始录制，编码" : @"结束录制，编码";
     [JRToast showWithText:toastStr duration:2];
-    
-    
 }
 
 #pragma mark - lazyload
